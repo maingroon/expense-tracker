7e14bd249b999b4c1f352e907fec1fc3a0f87796
Oleksandr Borovyk <maingroon@gmail.com>
2024-05-02 16:39:23 +0300

    Analytics screen

--- lib/screens/analytics/analytics_page.dart
+++ lib/screens/analytics/analytics_page.dart
@@ -1,4 +1,3 @@
-import 'package:expense_tracker/models/category_model.dart';
 import 'package:expense_tracker/services/transactions_service.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
